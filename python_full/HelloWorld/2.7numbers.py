5e2dfcfbb597ff4693a2e152cd498c4de5a98974
Marc Manley <marc@marcmanley.com>
2019-11-14 11:32:32 -0800

    working with numbers

---
+++ python_full/HelloWorld/2.7numbers.py
@@ -0,0 +1,27 @@
+# Numbers
+
+x = 1
+x = 1.1
+x = 1 + 2j  # a + bi (where 'i' is an imaginary number)
+
+# the above is an example of a complex number
+
+# let's do some math!
+
+print(10 + 3)
+print(10 - 3)
+print(10 * 3)
+print(10 / 3)
+print(10 // 3)  # this rounds the remainder
+print(10 % 3)  # modulus
+print(10 ** 3)  # exponent
+
+# augmented assignment operator
+
+x = 10
+# x = x + 3
+x += 3
+
+# the second two are the same
+
+print(x)
